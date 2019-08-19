178ce4eb057534666348d13c3045470b9b03b154
Vycheslav Tkachev <dragbarxd@gmail.com>
2019-08-19 17:49:26 +0300

    feat: implement creating charts

--- drawing-graphs/src/app/legend/legend.component.ts
+++ drawing-graphs/src/app/legend/legend.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { DataGraphService } from '../data-graph.service';
 
 @Component({
   selector: 'app-legend',
@@ -7,22 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LegendComponent implements OnInit {
 
-  color;
-  coordinates;
+  @ViewChild('coordinates', { static: false }) coordinates: ElementRef;
 
-  constructor() { }
+  color;
+  name: string;
+
+  constructor(private dataGraphService: DataGraphService) { }
 
   ngOnInit() {
     this.color = {
       hex: 16711684,
       hexString: '#FF0004',
-      hsl: {hue: 359.09289262156943, saturation: 100, lightness: 50},
-      rgb: {red: 255, green: 0, blue: 4}
+      hsl: { hue: 359.09289262156943, saturation: 100, lightness: 50 },
+      rgb: { red: 255, green: 0, blue: 4 }
     };
   }
 
-  changeCoordinates(event) {
-  // console.log(event.srcElement.value.split('\n'));
+  addChart() {
+    const coordinates = this.coordinates.nativeElement.value.split('\n');
+    const dataPoints = coordinates.map((elem) => {
+      const points = elem.split(':');
+      return { x: +points[0], y: +points[1] };
+    })
+    this.dataGraphService.graphs.push({
+      data: dataPoints,
+      color: this.color.hexString,
+      name: this.name
+    })
+  }
+
+  deleteChart(index) {
+    this.dataGraphService.graphs.splice(index, 1);
   }
 
 }
